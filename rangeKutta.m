530fe0d2c47fa535cd2f10637bb0ec9db64f38f1
AM0312 <ansh.midha0312@gmail.com>
2022-11-15 02:55:34 +0530

    updated ranga KUTTA

--- rangeKutta.m
+++ rangeKutta.m
@@ -1,16 +1,16 @@
-f=@(t,y)((2*t*y)+exp(t)/(t^(2)+t*exp(t)));
-t0=input('starting point');
-y0=input('function value at t0');
-h=input('step size');
-t1=input('point where f should be calculated');
+f=input("Enter a two variable function(y,t):");
+t0=input("Enter initial value:");
+y0=input("Enter initial function value:");
+t1=input("Enter value at which function is to be calculated:");
+h=input("Enter step size:");
 n=(t1-t0)/h;
-for i=1:n
+for i=1:n+1
     k1=h*f(t0,y0);
-    k2=h*f(t0+(h/2),y0+(k1/2));
-    k3=h*f(t0+(h/2),y0+(k2/2));
-    k4=h*f(t0+(h),y0+(k3));
-    y1=y0+((k1+(2*k2)+(2*k3)+k4)/6);
+    k2=h*f(t0+h/2,y0+k1/2);
+    k3=h*f(t0+h/2,y0+k2/2);
+    k4=h*f(t0+h,y0+k3);
+    y1=y0+(k1+2*k2+2*k3+k4)/6;
     t0=t0+h;
     y0=y1;
 end
-display(y1);
+disp(y1);
