eb876b3bf176e274c391f93c8bc1fb946630b511
AM0312 <ansh.midha0312@gmail.com>
2022-11-15 12:38:59 +0530

    Updated lagrange interpolation

--- lagrange.m
+++ lagrange.m
@@ -1,20 +1,18 @@
-function y=lagrange(x,xx,yy)
-n=size(xx,2);
-L=ones(n,size(x,2));
-if (size(xx,2)~=size(yy,2))
-    disp("Error");
-else
-   for i=1:n
-      for j=1:n
-         if (j~=i)
+xx=input("Enter values of x: ");
+yy=input("Enter values of function: ");
+x=input("Enter point to interpolate: ");
+n=length(xx);
+L=ones(n);
+for i=1:n
+    for j=1:n
+        if (j~=i)
             L(i,:)=L(i,:)*(x-xx(j))/(xx(i)-xx(j));
-         end
-      end
-   end
-   disp(L);
-   sum=0;
-   for i=1:n
-      sum=sum+yy(i)*L(i,:);
-   end
-   disp(sum);
+        end
+    end
 end
+disp(L);
+sum=0;
+for i=1:n
+    sum=sum+yy(i)*L(i,:);
+end
+disp(sum);
